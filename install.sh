db988297c2657e90ebb789e95585a44d9ba25121
오병준 <mail@luftaquila.io>
2023-03-17 13:02:12 +0900

    add: install zsh and powerline10k

--- install.sh
+++ install.sh
@@ -3,18 +3,23 @@
 packages=(
   "bat"
   "ctags"
-  "dust"
   "git"
   "htop"
   "powerline-go"
   "ripgrep"
   "thefuck"
   "tmux"
+  "zsh"
 )
-rust_packages=("git-delta")
+rust_packages=(
+  "git-delta"
+  "du-dust"
+  "tealdeer"
+)
 
 backups=(
   ".gitconfig"
+  ".p10k.zsh"
   ".tmux.conf"
   ".zshrc"
   ".bashrc"
@@ -42,6 +47,27 @@ if [ -e $1 ] || [ $1 != "skipinstall" ]; then
     printf "unknown os $os!\n"
     exit -1
   fi
+
+  # install Oh My Zsh
+  printf '\n\n####################################\n'
+  printf 'installing oh my zsh...'
+  printf '\n####################################\n\n'
+
+  echo "curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh"
+  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+  echo "chsh -s `which zsh`"
+  chsh -s `which zsh`
+
+
+  # install powerline10k
+  printf '\n\n####################################\n'
+  printf 'installing powerline10k...'
+  printf '\n####################################\n\n'
+
+  echo "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 
   # install RUST
   printf '\n\n####################################\n'
@@ -98,12 +124,10 @@ printf '\n\n####################################\n'
 printf 'generating symlinks to target...'
 printf '\n####################################\n\n'
 
-dir=`pwd`
-
 for obj in "${backups[@]}"
 do
-  echo "ln -s $dir/$obj ~/$obj"
-  ln -s $dir/$obj ~/$obj
+  echo "ln -s `pwd`/$obj ~/$obj"
+  ln -s `pwd`/$obj ~/$obj
 done
 
 
@@ -121,12 +145,5 @@ cp -r ./.vim ~
 echo "git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "vi +VundleInstall +qall"
-vi +VundleInstall +qall
-
-
-# install powerline10k
-printf '\n\n####################################\n'
-printf 'installing powerline10k...'
-printf '\n####################################\n\n'
-
+echo "vi -E -s -u ~/.vimrc +VundleInstall +qall"
+vi -E -s -u ~/.vimrc +VundleInstall +qall
