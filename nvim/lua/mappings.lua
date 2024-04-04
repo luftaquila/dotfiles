056da4b4a3bde8ad2a2b490619cb81830e0cbedf
오병준 <mail@luftaquila.io>
2024-04-05 02:42:31 +0900

    update: terminal command mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -13,3 +13,15 @@ map('n', 'tc', ':tabclose<CR>', { desc = "Tab close" })
 map('n', 'tl', ':tabnext<CR>', { desc = "Tab next" })
 map('n', 'th', ':tabprev<CR>', { desc = "Tab prev" })
 
+map('n', '<leader>rr', function ()
+  require("nvchad.term").new { pos = "float", size = 0.3 }
+
+  vim.api.nvim_chan_send(vim.b[0].channel, "ls\n")
+
+end, { desc = "RTWORKS build" })
+
+-- Floating terminal
+map("n", "<leader>a", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "Terminal Toggle Floating term" })
+
