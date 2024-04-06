32cbfc728465eb7b9a457283daff48390f242fcc
오병준 <mail@luftaquila.io>
2024-04-06 15:25:28 +0900

    fix: terminal buffer insert mode on window change

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,10 +40,27 @@ end)
 
 -- Restore cursor position
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-    pattern = { "*" },
-    callback = function()
-        vim.api.nvim_exec('silent! normal! g`"zv', false)
-    end,
+  pattern = { "*" },
+  callback = function()
+    vim.api.nvim_exec('silent! normal! g`"zv', false)
+  end,
+})
+
+-- auto enter/leave insert mode on terminal buffers
+vim.api.nvim_create_autocmd({"BufWinEnter", "WinEnter", "TermOpen"}, {
+  pattern = "term://*",
+  callback = function()
+    vim.cmd("startinsert")
+    vim.cmd('HideBadWhitespace')
+  end
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+  pattern = "term://*",
+  callback = function()
+    vim.cmd("stopinsert")
+    vim.cmd('ShowBadWhitespace')
+  end
 })
 
 -- session.vim
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -15,10 +15,10 @@ local function terminal_launch(mode, cmd, id, size)
   local ch
 
   if (id) then
-    require("nvchad.term").toggle { pos = mode, size = size and size or 0.3, id = id }
+    require("nvchad.term").toggle { pos = mode, size = size and size or 0.25, id = id }
     ch = vim.b[opts_to_id(id).buf].terminal_job_id
   else
-    require("nvchad.term").new { pos = mode, size = size and size or 0.3 }
+    require("nvchad.term").new { pos = mode, size = size and size or 0.25 }
     ch = vim.b[vim.api.nvim_get_current_buf()].terminal_job_id
   end
 
@@ -40,7 +40,7 @@ map('n', 'th', ':tabprev<CR>', { desc = "Tab prev" })
 
 -- terminals
 map('n', '<leader>v', function()
-  require("nvchad.term").toggle { pos = "vsp", id = "vertical" }
+  require("nvchad.term").toggle { pos = "vsp", id = "vertical", size = 0.25 }
 end, { desc = "Terminal Toggle vertical terminal" })
 
 map('n', '<leader>h', function()
@@ -52,8 +52,8 @@ map('n', '<leader>a', function()
 end, { desc = "Terminal Toggle Floating terminal" })
 
 -- Ctrl + P to previous window
-map('n', '<C-p>', '<C-w>p<ESC>', { desc = "Jump to previous window" })
-map('t', '<C-p>', '<C-\\><C-o><C-w>p<ESC>', { desc = "Jump to previous window" })
+map('n', '<C-p>', '<C-w>p', { desc = "Jump to previous window" })
+map('t', '<C-p>', '<C-\\><C-o><C-w>p', { desc = "Jump to previous window" })
 
 -- RTWORKS
 map('n', '<leader>sm', function ()
@@ -80,7 +80,7 @@ map('n', '<leader>le', function ()
   terminal_launch("sp", "le")
 end, { desc = "RTWORKS local build and execute" })
 
-map('n', '<leader>lr', function ()
+map({'n', 't'}, '<leader>lr', function ()
   local cmd = 'source ~/dotfiles/scripts/rtworks/commands.zsh;'
   vim.cmd('echom system(' .. "'zsh -c " .. '"' .. cmd .. 'fn_rtworks_local_run"' .. "')")
 end, { desc = "RTWORKS local launch" })
