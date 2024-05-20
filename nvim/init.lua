2b239ca69d0f333f95851632627426bf49272845
오병준 <mail@luftaquila.io>
2024-05-20 13:22:37 +0900

    neovim: replace deprecated nvim_exec

--- nvim/init.lua
+++ nvim/init.lua
@@ -62,7 +62,7 @@ vim.api.nvim_command('set modeline')
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
   pattern = { "*" },
   callback = function()
-    vim.api.nvim_exec('silent! normal! g`"zv', false)
+    vim.cmd('silent! normal! g`"zv')
   end,
 })
 
