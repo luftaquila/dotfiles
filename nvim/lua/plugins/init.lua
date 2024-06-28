d26bb77fee7a9704c505ac57f2629f45b3852931
오병준 <mail@luftaquila.io>
2024-06-28 14:45:04 +0900

    neovim: update specs configuration

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -274,15 +274,21 @@ return {
     config = function()
       require("specs").setup {
         show_jumps = true,
-        min_jump = 3,
+        min_jump = 2,
         popup = {
           delay_ms = 0,
-          inc_ms = 10,
-          blend = 30,
-          width = 20,
+          inc_ms = 8,
+          blend = 50,
+          width = 8,
           winhl = "PMenuSel",
-          fader = require("specs").pulse_fader,
-          resizer = require("specs").shrink_resizer,
+          fader = require("specs").exp_fader,
+          resizer = function(width, ccol, cnt)
+            if cnt <= width then
+              return { width + cnt + 1, ccol - (width + cnt) / 2 }
+            else
+              return nil
+            end
+          end,
         },
         click_to_move = true,
         move_to_insert = true,
