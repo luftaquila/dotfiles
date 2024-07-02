9afcfd07307f9fdad84d6d20af79faaa2735de1a
오병준 <mail@luftaquila.io>
2024-07-02 13:34:10 +0900

    nvim: update telescope keymaps

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -40,16 +40,20 @@ map("n", "<leader>gt", telescope.git_stash, { desc = "Telescope Git Stash" })
 map("n", "<leader>ca", require("actions-preview").code_actions, { desc = "Telescope LSP code action preview" })
 map("n", "<leader>fl", telescope.diagnostics, { desc = "Telescope find LSP diagnostics" })
 
+map("n", "<leader>fc", telescope.commands, { desc = "Telescope find commands" })
 map("n", "<leader>fr", telescope.registers, { desc = "Telescope find registers" })
 map("n", "<leader>fu", telescope.grep_string, { desc = "Telescope grep string under cursor" })
-map("n", "<leader>fz", telescope.current_buffer_fuzzy_find, { desc = "Telescope find current buffer" })
+map("n", "<leader>fb", telescope.current_buffer_fuzzy_find, { desc = "Telescope find current buffer" })
 
 map("n", "<leader>fk", telescope.keymaps, { desc = "Telescope find keymaps" })
 map("n", "<leader>fj", telescope.marks, { desc = "Telescope find marks" })
-map("n", "<leader>ft", telescope.filetypes, { desc = "Telescope find filetypes" })
+map("n", "<leader>fp", telescope.filetypes, { desc = "Telescope find filetypes" })
 map("n", "<leader>fs", telescope.spell_suggest, { desc = "Telescope spell suggestion" })
 
-map("n", "<leader>fw", function()
+map("n", "<leader>ft", telescope.lsp_dynamic_workspace_symbols, { desc = "Telescope find LSP tags" })
+
+map("n", "<leader>fw", telescope.live_grep, { desc = "Telescope live grep" })
+map("n", "<leader>fz", function()
   telescope.grep_string { search = "" }
 end, { desc = "Telescope fuzzy grep string" })
 
