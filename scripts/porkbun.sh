ff197b64ac0737f8f85b1f01a6effebff8b3c22f
오병준 <mail@luftaquila.io>
2025-05-08 14:02:42 +0900

    porkbun: portable ssl script

--- scripts/porkbun.sh
+++ scripts/porkbun.sh
@@ -16,12 +16,10 @@ echo "Removing original *.pem files"
 sudo rm /etc/ssl/porkbun/luftaquila.io/*.pem
 
 echo "Copying new *.pem files"
+sudo mkdir -p /etc/ssl/porkbun/luftaquila.io
 sudo cp *.pem /etc/ssl/porkbun/luftaquila.io
 
 echo "Removing temporary files"
 rm *.pem
-
-echo "Restarting web server"
-sudo systemctl restart nginx
 
 echo "Job completed"
