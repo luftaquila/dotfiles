5819ecf364f7357a6a26a31a597d6c1574c99907
오병준 <mail@luftaquila.io>
2025-07-04 16:08:47 +0900

    nvim: indent-blankline integration with rainbow-delimiters

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -316,16 +316,31 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    opts = function(_, opts)
-      opts = {
-        indent = { char = " " },
-        scope = { enabled = false },
+    config = function(_, opts)
+      local highlight = { "RainbowRed", "RainbowBlue", "RainbowOrange", "RainbowGreen", "RainbowViolet", "RainbowCyan" }
+
+      local hooks = require "ibl.hooks"
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "IndentGray", { fg = vim.api.nvim_get_hl(0, { name = "CursorLine" }).bg })
+      end)
+
+      vim.g.rainbow_delimiters = { highlight = highlight }
+
+      require("ibl").setup {
+        indent = { highlight = { "IndentGray" } },
+        scope = { highlight = highlight },
+        whitespace = { remove_blankline_trail = true },
       }
-      return require("indent-rainbowline").make_opts(opts)
+
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
-    dependencies = {
-      "TheGLander/indent-rainbowline.nvim",
-    },
+    dependencies = { "HiPhish/rainbow-delimiters.nvim" },
   },
 
   {
