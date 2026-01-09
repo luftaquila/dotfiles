2edbed7c936e68fa308958a84ba214c102dfa1a9
오병준 <mail@luftaquila.io>
2026-01-10 02:09:38 +0900

    nvchad: use official repo

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,7 +16,7 @@ local lazy_config = require "configs.lazy"
 -- load plugins ---------------------------------------------------------------
 require("lazy").setup({
   {
-    "luftaquila/NvChad",
+    "NvChad/NvChad",
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
